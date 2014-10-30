e8c28efc3c1ac011d68e289b442676990cd21e27
Kamaleshwar <bnkamalesh@hotmail.com>
2014-10-30 23:39:12 +0530

    Update tooltip.js
    Improved accuracy, improved performance. Added new feature, you can now add a custom class to the tool tip container based on the target element.

--- tooltip.js
+++ tooltip.js
@@ -1,153 +1,154 @@
-var $ = jQuery;
-var tttext = new function() {
-
+var tttext = (function() {
 	// The following variables save the default values
 
-	var tttinstance=this, type="hover", duration = 300, position = "bottom", offset = 0, 
-		horizontal_centre = true, vertical_centre = true,
-		condition = function(){return true;}, 
-		tooltip_point = $('<span class="point"></span>'), ttcontainer = $('<div id="tooltip"></div>');
+	var tooltip_point = $('<span class="point"></span>'), tinst = null,
+		ttcontainer = $('<div id="tooltip"></div>'), html_cont = $("html"),
+		win_height = $(window).outerHeight(true), doc_height = html_cont.outerHeight(true), options = {};
+
 
 	/* 
 	tooltip_point: This is used simply to display the pointer
 	ttcontainer: The container in which tooltip is displayed
+	tinst: This instance
 	*/
 
-	tttinstance.init = function (options) {
-		if(!options || !options.container)
-			$('body').append(ttcontainer);
-		tttinstance.setDefaults(options);
-	};
+	return {
+		init: function (opts) {
+				tinst = this;
+				if(!opts)
+					opts = {};
 
-	// setDefaults can be called explicitly to set the default behaviour throughout the page.
-	tttinstance.setDefaults = function(options) {
-		if(options) {
-			type = options.type || ''; // Type of action with which tool tip is triggered. Available types: "hover", "click", "follow". Default: "hover"
-			position = options.position || position; // Position at which the tooltip appears. Available positions: "top", "right", "bottom", "left". Default: "bottom"
-			duration = options.duration || duration; // Animation duration. Default: 300
-			ttcontainer = options.container || ttcontainer; // Tooltip container
-			offset = options.offset || offset; // Offset. Tooltip appears 'offset' pixels away from the actual position. Default: 0
-			horizontal_centre = options.horizontal_centre || horizontal_centre; // If true, tooltip appears at the horizontal centre of the target element. Default: true
-			vertical_centre = options.vertical_centre || vertical_centre; // If true, tooltip appears at the vertical centre of the target element. Default: true
-			condition = options.condition || condition; // A function, which will be checked before displaying the tooltip
+				options = {
+					type: opts.type || "hover", // Type of action with which tool tip is triggered. Available types: "hover", "click", "follow". Default: "hover"
+					position: opts.position || "top", // Position at which the tooltip appears. Available positions: "top", "right", "bottom", "left". Default: "bottom"
+					duration: opts.duration || 300, // Animation duration. Default: 300
+					offset: opts.offset || 0, // Offset. Tooltip appears 'offset' pixels away from the actual position. Default: 1
+					horizontal_centre: (typeof opts.horizontal_centre=="undefined")? true : opts.horizontal_centre, // If true, tooltip appears at the horizontal centre of the target element. Default: true
+					vertical_centre: (typeof options.vertical_centre=="undefined")? true : opts.vertical_centre, // If true, tooltip appears at the vertical centre of the target element. Default: true
+					condition: (typeof options.condition=="function")? options.condition : function(){ return true; }, // A function, which will be checked before displaying the tooltip
+					allow_body_overflow: (typeof options.allow_body_overflow=="undefined")? true :  options.allow_body_overflow,
+					custom_class: opts.custom_class || ""
+				};
+
+				if($("body #tooltip").length===0)
+					$('body').append(ttcontainer);
+		},
+		attach: function(item, opts) {
+			if(!item) return;
+
+			item.data("type", opts.type || options.type);
+			item.data("duration", opts.duration || options.duration);
+			item.data("position", opts.position || options.position);
+			item.data("offset", opts.offset || options.offset);
+			item.data("condition", opts.condition || options.condition);
+			item.data("message", opts.message); // Content of the Tooltip, this can be HTML too.
+			item.data("h_centre", (typeof opts.horizontal_centre=="undefined") ? options.horizontal_centre : opts.horizontal_centre );
+			item.data("v_centre", (typeof opts.vertical_centre=="undefined") ? options.vertical_centre : opts.vertical_centre);
+			item.data("allow_body_overflow", (typeof opts.allow_body_overflow=="undefined") ? opts.allow_body_overflow : options.allow_body_overflow);
+			item.data("custom_class", opts.custom_class || options.custom_class);
+
+			// type: used to set the action upon which the tooltip appears
+			switch( item.data("type") ) {
+				case "follow": // tooltip follows the cursor
+					item.mousemove(function(e) {
+						item.data("position", e.pageX+"#"+e.pageY);
+						tinst.show($(this));
+					});
+
+					item.mouseout(function() {
+						tinst.hide(type="follow");
+					});
+				break;
+
+				case "click":
+					item.click(function() {
+						( ttcontainer.hasClass("active") )? tinst.hide(): tinst.show( $(this) );
+					});
+
+				break;
+
+				default:
+					// Default type, hover
+					item.hover(function() {
+						tinst.show( $(this) );
+					}, function() {
+						tinst.hide();
+					});
+			}
+			return item;
+		},
+		show: function(item) {
+			// show the tool tip container
+			if(!item.data("condition").call()) return item;
+
+			var item_offset = item.offset(), left=item_offset.left, top=item_offset.top,item_type = item.data("type"),
+				pos_class=((item_type=="follow")?item_type:item.data("position"));
+
+			ttcontainer.html(item.data("message")).append(tooltip_point).attr("class","active "+pos_class+" "+item.data("custom_class"));
+
+			switch(item.data("position")) {
+				case "left":
+					left -= (ttcontainer.outerWidth(true)+item.data("offset"));
+					if(item.data("v_centre")) {
+						var h = item.outerHeight(true) - (parseInt(item.css("margin-bottom")));
+						h = (h/2) - (ttcontainer.outerHeight(true) / 2);
+						if(h<0)
+							h*=-1;
+						top+=h;
+					}
+					else
+						top += ( parseInt(item.css("padding-top")) - parseInt(ttcontainer.css("padding-top")) );
+				break;
+
+				case "top":
+					top -= (ttcontainer.outerHeight(true)+item.data("offset"));
+
+					(item.data("h_centre")===true)? left += ( ( (item.outerWidth(true) - (parseInt(item.css("margin-left")) + parseInt(item.css("margin-right")) ) ) - ttcontainer.outerWidth(true) ) / 2) : 
+					left -= ttcontainer.outerWidth(true) /2;
+					
+				break;
+
+				case "right":
+					left += (item.outerWidth(true) - parseInt(item.css("margin-right")) )+item.data("offset");
+					if(item.data("v_centre")) {
+						var h = item.outerHeight(true) - (parseInt(item.css("margin-bottom")));
+						h = (h/2) - (ttcontainer.outerHeight(true) / 2);
+						if(h<0)
+							h*=-1;
+						top+=h;
+					}
+					else
+						top += ( parseInt(item.css("padding-top")) - parseInt(ttcontainer.css("padding-top")) );
+
+				break;
+
+				default: //bottom
+					var split_pos = item.data("position").split("#"); // Split is used incase of follow, to get x,y of cursor.
+
+					if( split_pos[1] ) {
+						left = parseInt(split_pos[0]) + 5;
+						top = parseInt(split_pos[1]) + 5;
+					}
+					else {
+						left = (item.data("h_centre"))? left += ( (item.outerWidth(true) / 2) - (ttcontainer.outerWidth(true) / 2) ): left;
+						top += (item.outerHeight(true) - parseInt(item.css("margin-bottom")) ) + item.data("offset");
+					}
+			}
+
+			ttcontainer.css("left", (left<0) ? 0 : left);
+			ttcontainer.css("top", (top<0) ? 0 : top);
+
+			tooltip_point.css("left", (ttcontainer.outerWidth(true)/2) - (tooltip_point.outerWidth(true)/2) );
+
+			if((win_height > doc_height) && item.data("allow_body_overflow")===false )
+				html_cont.addClass("no-overflow");
+
+			(item_type=="follow")? ttcontainer.stop(true, true).fadeIn(item.data("duration")) : ttcontainer.stop(true, true).fadeIn(item.data("duration"));
+		},
+		hide: function(type) {
+			html_cont.removeClass("no-overflow");
+			var opt = (type=="follow")? false : true;
+			ttcontainer.stop(opt, opt).fadeOut(options.duration).removeClass("active");
 		}
 	};
-
-	// attach() is used to attach the tool tip to required elements
-	tttinstance.attach = function(item, options) {
-		item.data("type", options.type || type);
-		item.data("duration", options.duration || duration);
-		item.data("position", options.position || position);
-		item.data("offset", options.offset || offset);
-		item.data("condition", options.condition || condition);
-		item.data("message", options.message); // Content of the Tooltip, this can be HTML too.
-		item.data("h_centre", (typeof options.horizontal_centre == "undefined")? horizontal_centre : options.horizontal_centre);
-		item.data("v_centre", (typeof options.vertical_centre == "undefined")? vertical_centre : options.vertical_centre);
-
-		// type: used to set the action upon which the tooltip appears
-		switch( item.data("type") ) {
-			case "follow": // tooltip follows the cursor
-				item.mousemove(function(e) {
-					$(this).data("position", e.pageX+"#"+e.pageY);
-					tttinstance.show($(this));
-					return;
-				});
-
-				item.mouseout(function() {
-					tttinstance.hide(type="follow");
-					return;
-				});
-			break;
-
-			case "click":
-				item.click(function() {
-					( ttcontainer.hasClass("active") )? tttinstance.hide(): tttinstance.show( $(this) );
-					return;
-				});
-
-			break;
-
-			default:
-				item.hover(function() {
-					tttinstance.show( $(this) );
-					return;
-				}, function() {
-					tttinstance.hide();
-					return;
-				});
-		}
-		return item;
-	};
-
-	// show the tool tip container
-	tttinstance.show = function(item) {
-		if(!item.data("condition")()) return item;
-
-		var item_offset = item.offset(), left=item_offset.left, top=item_offset.top,
-			i_offset = item.data("offset"), h_centre = item.data("h_centre"),
-			v_centre = item.data("v_centre"), pos = item.data("position"), item_type = item.data("type"),
-			item_height = item.outerHeight(true), item_width = item.outerWidth(true), pos_class=((item_type=="follow")?item_type:pos);
-
-		ttcontainer.html(item.data("message")).append(tooltip_point).attr("class","active "+pos_class);
-
-		switch(pos) {
-			case "left":
-				left -= (ttcontainer.outerWidth(true)+i_offset);
-				if(v_centre) {
-					var h = item_height - (parseInt(item.css("margin-bottom")));
-					h = (h/2) - (ttcontainer.outerHeight(true) / 2);
-					if(h<0)
-						h*=-1;
-					top+=h;
-				}
-				else
-					top += ( parseInt(item.css("padding-top")) - parseInt(ttcontainer.css("padding-top")) );
-			break;
-
-			case "top":
-				top -= (ttcontainer.outerHeight(true)+i_offset);
-
-				(h_centre===true)? left += ( ( (item_width - (parseInt(item.css("margin-left")) + parseInt(item.css("margin-right")) ) ) - ttcontainer.outerWidth(true) ) / 2) : 
-				left -= ttcontainer.outerWidth(true) /2;
-				
-			break;
-
-			case "right":
-				left += (item_width - parseInt(item.css("margin-right")) )+i_offset;
-				if(v_centre) {
-					var h = item_height - (parseInt(item.css("margin-bottom")));
-					h = (h/2) - (ttcontainer.outerHeight(true) / 2);
-					if(h<0)
-						h*=-1;
-					top+=h;
-				}
-				else
-					top += ( parseInt(item.css("padding-top")) - parseInt(ttcontainer.css("padding-top")) );
-
-			break;
-
-			default: //bottom
-				var split_pos = item.data("position").split("#"); // Split is used incase of follow, to get x,y of cursor.
-
-				if( split_pos[1] ) {
-					left = parseInt(split_pos[0]) + 5;
-					top = parseInt(split_pos[1]) + 5;
-				}
-				else {
-					left = (h_centre)? left += ( (item_width / 2) - (ttcontainer.outerWidth(true) / 2) ): left;
-					top += (item_height - parseInt(item.css("margin-bottom")) ) + i_offset;
-				}
-		}
-
-		ttcontainer.css("left", (left<0) ? 0 : left);
-		ttcontainer.css("top", (top<0) ? 0 : top);
-
-		
-		(item_type=="follow")? ttcontainer.fadeIn(duration) : ttcontainer.stop(true, true).fadeIn(duration);
-	}
-
-	tttinstance.hide = function(type) {
-		var opt = (type=="follow")? false : true;
-		ttcontainer.stop(opt, opt).fadeOut(duration).removeClass("active");
-	}
-};
+}());
